ce351ee32fd6464d512a9f358d5be9e9564f99d7
mavb86 <57711930+mavb86@users.noreply.github.com>
2020-06-11 20:38:23 +0200

    Add files via upload

---
+++ RS232DotNet.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+
+        Try
+            Dim data As String
+            data = SerialPort1.ReadExisting()
+            tDatos.AppendText(data)
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Control.CheckForIllegalCrossThreadCalls = False
+        
+        If SerialPort1.IsOpen = True Then
+            SerialPort1.Close()
+            SerialPort1.Open()
+        Else
+            SerialPort1.Open()
+        End If
+
+    End Sub
+
+End Class
